f6b1b0f3c7ded2f6a258b95c59057d7e562eeba9
Xadman <90847817+Xadman@users.noreply.github.com>
2022-04-05 10:49:27 -0500

    added volume and Background sound

---
+++ Assets/Scripts/PrefrencesManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefrencesManager : MonoBehaviour
+{
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat("MusicVolume", 1);
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat("MasterVolume", 1);
+    }
+
+    public static void SetMusicVolume(float soundLevel)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", soundLevel);
+    }
+
+     public static void SetMasterVolume(float soundLevel)
+     {
+      PlayerPrefs.SetFloat("MasterVolume", soundLevel);
+     }
+}
